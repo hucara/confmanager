92ebe650497f0be45010934e100a30abba299978
hugoc <zeroshcr@gmail.com>
2012-01-30 14:08:33 +0100

    Hello world commit!

---
+++ Configuration Manager/CustomControls/CLabel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Configuration_Manager.CustomControls
+{
+    class CLabel : Label, ICustomControl
+    {
+    }
+}
---
+++ Configuration Manager/CustomControls/ICustomControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Configuration_Manager.CustomControls
+{
+    interface ICustomControl
+    {
+        public Control c;
+        public List<Control> RelatedControls;
+    }
+}
